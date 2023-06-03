6fa5dc7a3327c5110159b2939fd4341b5729da48
= <=>
2023-06-03 10:54:04 +0800

    adding themes

--- app/src/main/java/com/themovieguide/org/ui/theme/Color.kt
+++ app/src/main/java/com/themovieguide/org/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.themovieguide.org.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Purple200 = Color(0xFFBB86FC)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
+val Purple700 = Color(0xFF3700B3)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey50 = Color(0xFF625b95)
 val Pink40 = Color(0xFF7D5260)
+val Teal200 = Color(0xFF03DAC5)
+val Purple500 = Color(0xFF6200EE)
+val PinkColor700 = Color(0xFFFF0058)
+val Gray800 = Color(0xFF808080)
+val LightDark220 = Color(0xFF222229)
+val BlackOpaque900 = Color(0xFF000000)
+val Default800 = Color(0xFF808080)
+val Primary800 = Color(0xFFFF0058)
